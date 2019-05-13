db243a82dc2ff0883c0aea520a3307ff96248901
boa <boa.matule@gmail.com>
2019-05-13 13:23:37 +0200

    Imports the DisplayResult component and render it in the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import DisplayResult from './Components/displayResult'
 
-class App extends Component() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+  
   render() {
     return (
       <div>
@@ -14,6 +23,11 @@ class App extends Component() {
           <label>Height(cm)</label>
           <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })}/>
         </div>
+
+        <DisplayResult
+          weight={this.state.weight}
+          height={this.state.height}
+        />
       </div>
     );
   }
