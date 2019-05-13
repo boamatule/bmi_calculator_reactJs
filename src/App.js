e00b31c970f11c46964c63a2c8c1cbde2e1670c1
boa <boa.matule@gmail.com>
2019-05-13 12:21:44 +0200

    added 2 input fields for the weight and height

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component() {
   render() {
     return (
       <div>
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" />
+        </div>
 
+        <div>
+          <label>Height(cm)</label>
+          <input name="height" />
+        </div>
       </div>
     );
   }
